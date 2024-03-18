08f02dd9bf7d4356fd874e25547a6fe06ab4e2db
Avidas1 <133372317+Avidas1@users.noreply.github.com>
2024-03-18 18:37:23 +0530

    updated

---
+++ app/Http/Controllers/AssignVehicleController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AssignVehicle;
+use App\Http\Requests\StoreAssignVehicleRequest;
+use App\Http\Requests\UpdateAssignVehicleRequest;
+
+class AssignVehicleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAssignVehicleRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AssignVehicle $assignVehicle)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AssignVehicle $assignVehicle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAssignVehicleRequest $request, AssignVehicle $assignVehicle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AssignVehicle $assignVehicle)
+    {
+        //
+    }
+}
