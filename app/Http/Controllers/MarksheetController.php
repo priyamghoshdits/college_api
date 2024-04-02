937c8a63b6efdc17ef5a245ee75e02b0ff93ebd7
Avidas1 <133372317+Avidas1@users.noreply.github.com>
2024-04-02 18:42:20 +0530

    updated

---
+++ app/Http/Controllers/MarksheetController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Marksheet;
+use App\Http\Requests\StoreMarksheetRequest;
+use App\Http\Requests\UpdateMarksheetRequest;
+
+class MarksheetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMarksheetRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Marksheet $marksheet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Marksheet $marksheet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMarksheetRequest $request, Marksheet $marksheet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Marksheet $marksheet)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_04_02_102550_create_marksheets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('marksheets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->foreignId('semester_id')->references('id')->on('semesters')->onDelete('cascade');
+            $table->foreignId('subject_id')->references('id')->on('subjects')->onDelete('cascade');
+            $table->foreignId('student_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('marks')->nullable(false);
+            $table->integer('full_marks')->nullable(false);
+            $table->integer('grade')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('marksheets');
+    }
+};
