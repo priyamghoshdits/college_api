daff495488a08b035c444b1d3e98e7424b5968e6
unknown <priyamghosh.dits@gmail.com>
2024-05-25 16:39:10 +0530

    updated

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Expense;
 use App\Models\LibraryStock;
 use App\Models\Payment;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
@@ -26,11 +27,23 @@ class DashboardController extends Controller
 
         $total_expense = Expense::sum('amount');
 
+        $total_student = count(User::whereUserTypeId(3)->get());
+
+        $total_teacher = count(User::whereUserTypeId(2)->get());
+
+        $total_male_student = count(User::whereUserTypeId(3)->whereGender('Male')->get());
+
+        $total_female_student = count(User::whereUserTypeId(3)->whereGender('Male')->get());
+
         $ret = [
             'total_books' => $total_books,
             'no_of_fees_received' => $no_of_fees_received,
             'total_fees_received' => $total_fees_received,
-            'total_expense' => $total_expense
+            'total_expense' => $total_expense,
+            'total_student' => $total_student,
+            'total_teacher' => $total_teacher,
+            'total_male_student' => $total_male_student,
+            'total_female_student' => $total_female_student
         ];
         return response()->json(['success'=>1,'data'=>$ret], 200,[],JSON_NUMERIC_CHECK);
     }
