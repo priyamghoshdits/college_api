32647f334c34d32c6201349455f8832a8e1bf0b1
Devant Marketing <priyamghosh73@gmail.com>
2024-08-29 09:04:16 +0530

    updated

--- app/Http/Controllers/StudentResultController.php
+++ app/Http/Controllers/StudentResultController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\UniversitySynopsisResource;
 use App\Models\StudentResult;
-use App\Http\Requests\StoreStudentResultRequest;
-use App\Http\Requests\UpdateStudentResultRequest;
 use Illuminate\Http\Request;
 
 class StudentResultController extends Controller
