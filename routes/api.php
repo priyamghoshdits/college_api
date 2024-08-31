5439e572a1722cbb3eec6a54ba67f61e0a03800f
Devant Marketing <priyamghosh73@gmail.com>
2024-08-31 14:04:11 +0530

    updated

--- routes/api.php
+++ routes/api.php
@@ -127,7 +127,7 @@ Route::get("forgotPassword/{email_id}", [UserController::class, 'forgot_password
 Route::get("initilialize", [RolesAndPermissionController::class, 'initilialize']);
 Route::get("clearCache", [RolesAndPermissionController::class, 'clear_cache']);
 
-Route::get("appUpdateChecker", [AppVersionController::class, 'check_app_version']);
+Route::post("appUpdateChecker", [AppVersionController::class, 'check_app_version']);
 
 
 Route::get("testF", [MemberController::class, 'testUser']);
